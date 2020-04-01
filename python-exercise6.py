225db20b670c86e5e297f347d3203aba08e93ee9
David Foster <dlfosterii@yahoo.com>
2020-04-01 15:33:21 -0400

    exercise 6 complete

--- python-exercise6.py
+++ python-exercise6.py
@@ -2,5 +2,10 @@
 
 # user input tempature
 
-temp = int(input(f'Enter a tempature in Celsius: '))
+celsius_temp = int(input(f'Enter a tempature in Celsius: '))
+
+fahernheit_temp = (celsius_temp * (9/5)) + 32
+
+print(f'That\'s {fahernheit_temp} Fahernheit')
+
 
