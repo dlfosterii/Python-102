40f4ed351ee6177dba335da425d7e1c3953ad9ca
David Foster <dlfosterii@yahoo.com>
2020-04-01 16:11:22 -0400

    updated exercise 6

--- python-exercise6.py
+++ python-exercise6.py
@@ -1,11 +1,10 @@
 # A tempature conversion tool
 
 # user input tempature
-
 celsius_temp = int(input(f'Enter a tempature in Celsius: '))
 
+#convert tempature
 fahernheit_temp = (celsius_temp * (9/5)) + 32
 
+#output the final tempature
 print(f'That\'s {fahernheit_temp} Fahernheit')
-
-
