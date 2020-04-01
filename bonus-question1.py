860313288f0d35fd0b3ddd75610c0d9ac2655ac4
David Foster <dlfosterii@yahoo.com>
2020-04-01 17:17:06 -0400

    Bonus question completion

---
+++ bonus-question1.py
@@ -0,0 +1,13 @@
+#Use only one variable, but prompt the user for three pieces of input:
+
+#First pet's name
+#Street you grew up on
+#An adjective
+#Then, print the variable in this format:
+
+#"Hello, the Amazing Cleopatra Peachtree!"
+
+
+content = f"{input(f'First pets name? ')} {input(f'Street you grew up on? ')}"
+
+print(f'Hello, wonderful {content}!')
